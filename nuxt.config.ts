e833b64fffdabfb667fca96f3f51342c6a8e2f4e
faiqnoov <mfaiqn@gmail.com>
2024-07-04 21:40:49 +0700

    setup shadcn

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  compatibilityDate: "2024-07-04"
+	devtools: { enabled: true },
+	compatibilityDate: "2024-07-04",
+	modules: ["@nuxtjs/tailwindcss", "shadcn-nuxt"],
+	shadcn: {
+		/**
+		 * Prefix for all the imported component
+		 */
+		prefix: '',
+		/**
+		 * Directory that the component lives in.
+		 * @default "./components/ui"
+		 */
+		componentDir: './components/ui'
+	}
 })
