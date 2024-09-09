7fdca14331fb1c4837c0f168315833f4c2bf4c91
faiqnoov <mfaiqn@gmail.com>
2024-09-09 22:33:17 +0700

    draft: sidebar & page layout

---
+++ components/sidebar/Main.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+import { ChevronLeftIcon } from '@radix-icons/vue'
+
+const sidebarItem = [
+	{
+		title: 'Home',
+		url: '/',
+		icon: '',
+	},
+	{
+		title: 'Datatable',
+		url: '/datatable',
+		icon: '',
+	},
+	{
+		title: 'Profile',
+		url: '',
+		icon: '',
+	},
+]
+</script>
+
+<template>
+	<div class="fixed top-0 left-0 right-0 h-14 shadow z-10"></div>
+
+	<div class="fixed top-0 bottom-0 left-0 w-64 bg-white p-4 shadow-xl z-20">
+		<Button variant="ghost" size="icon" class="absolute top-2.5 -right-12">
+			<ChevronLeftIcon class="w-4 h-4" />
+		</Button>
+
+		<div class="w-full text-xl text-center">
+			<h1>Application Name</h1>
+		</div>
+
+		<div class="flex flex-col gap-2 mt-10">
+			<Button variant="ghost" class="text-left" v-for="item in sidebarItem" @click="navigateTo(item.url)">
+				{{ item.title }}
+			</Button>
+		</div>
+	</div>
+
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="w-full h-full">
+		<!-- sidebar -->
+		<SidebarMain></SidebarMain>
+
+		<!-- main container -->
+		<div class="ml-64 pt-14 h-screen">
+			<div class="flex flex-col h-full overflow-y-auto bg-white">
+				<!-- content -->
+				<div class="px-6 py-4 flex-1">
+					<slot />
+				</div>
+
+				<!-- footer -->
+				<div v-if="true" class="flex justify-end py-2 px-3 w-full border-t text-slate-300">
+					<span class="text-xs">&copy; noovproject 2024</span>
+				</div>
+			</div>
+
+		</div>
+	</div>
+</template>
