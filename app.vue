58e7eae2b892167225b57e8d9cab0c17edb5f0e8
faiqnoov <mfaiqn@gmail.com>
2024-11-04 20:33:09 +0700

    feat: add shadcn form & accordion component; add some packages

--- app.vue
+++ app.vue
@@ -1,3 +1,11 @@
+<script setup>
+// onMounted(() => {
+//   const scriptTag = document.createElement("script");
+//   scriptTag.src = "https://unpkg.com/@phosphor-icons/web";
+//   document.getElementsByTagName('head')[0].appendChild(scriptTag);
+// })
+</script>
+
 <template>
   <NuxtLoadingIndicator color="#363636" />
   <NuxtLayout>
